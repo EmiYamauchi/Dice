76ad621fd302d31383e47ca9dfc05681f8bc8de7
Emi Yamauchi <s1280176@u-aizu.ac.jp>
2021-07-02 16:24:00 +0900

    Initial revision

---
+++ Dice1.java
@@ -0,0 +1,15 @@
+class Dice1{
+    public static void main(String[] args){
+
+        System.out.println("What is your name?");
+        String s = new java.util.Scanner(System.in).nextLine()
+        System.out.println("Hello," + s + "!");
+        int r1 = new java.util.Random().nextInt(6) + 1;
+        int r2 = new java.util.Random().nextInt(6) + 1;
+
+        System.out.println("Rolling the dice...");
+        System.out.println("Die 1: " + r1);
+        System.out.println("Die 1: " + r2);
+        System.out.println("Total value: " + (r1+r2));
+    }
+}
