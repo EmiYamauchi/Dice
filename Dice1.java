9b8cda59b80a389bf71ac9eff92dd7d9de346fd9
Emi Yamauchi <s1280176@u-aizu.ac.jp>
2021-07-05 11:53:06 +0900

    Won/lost message modified

--- Dice1.java
+++ Dice1.java
@@ -1,12 +1,9 @@
 class Dice1{
     public static void main(String[] args){
-<<<<<<< HEAD
-=======
 
         System.out.println("What is your name?");
-        String s = new java.util.Scanner(System.in).nextLine()
+        String s = new java.util.Scanner(System.in).nextLine();
         System.out.println("Hello," + s + "!");
->>>>>>> experimental
         int r1 = new java.util.Random().nextInt(6) + 1;
         int r2 = new java.util.Random().nextInt(6) + 1;
 
@@ -14,14 +11,11 @@ class Dice1{
         System.out.println("Die 1: " + r1);
         System.out.println("Die 1: " + r2);
         System.out.println("Total value: " + (r1+r2));
-<<<<<<< HEAD
 
         if(r1+r2>7){
             System.out.println(s + " won!");
         }else{
             System.out.println(s + " lost");
         }
-=======
->>>>>>> experimental
     }
 }
