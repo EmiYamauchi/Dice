d2bbcddd98f42ad2860f236922426408cd30b30c
Emi Yamauchi <s1280176@u-aizu.ac.jp>
2021-07-05 11:57:12 +0900

    Greetings added

--- Dice1.java
+++ Dice1.java
@@ -2,7 +2,7 @@ class Dice1{
     public static void main(String[] args){
 
         System.out.println("What is your name?");
-        String s = new java.util.Scanner(System.in).nextLine()
+        String s = new java.util.Scanner(System.in).nextLine();
         System.out.println("Hello," + s + "!");
         int r1 = new java.util.Random().nextInt(6) + 1;
         int r2 = new java.util.Random().nextInt(6) + 1;
